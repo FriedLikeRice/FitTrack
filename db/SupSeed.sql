bd0093e31a05572e089f72a2cf091a8e44273af6
FriedLikeRice <jacobleeferraro@gmail.com>
2024-04-02 18:40:52 -0700

    added basic seed files for sql

---
+++ db/SupSeed.sql
@@ -0,0 +1,29 @@
+-- Create table for supplements
+CREATE TABLE supplements (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    serving_size VARCHAR(50) NOT NULL
+);
+
+-- Inserting the top 20 workout supplements and their serving sizes
+INSERT INTO supplements (name, serving_size) VALUES 
+    ('Whey Protein', '1 scoop (30g)'),
+    ('Creatine Monohydrate', '5g'),
+    ('BCAAs (Branched-Chain Amino Acids)', '1 scoop (10g)'),
+    ('Pre-Workout', '1 scoop (15g)'),
+    ('Glutamine', '5g'),
+    ('Casein Protein', '1 scoop (30g)'),
+    ('Fish Oil', '1 capsule (1000mg)'),
+    ('Multivitamin', '1 tablet'),
+    ('Beta-Alanine', '2g'),
+    ('Caffeine', '200mg'),
+    ('L-Glutamine', '5g'),
+    ('Vitamin D3', '5000 IU'),
+    ('ZMA (Zinc Magnesium Aspartate)', '3 capsules'),
+    ('HMB (Hydroxy Methylbutyrate)', '3g'),
+    ('L-Carnitine', '500mg'),
+    ('Omega-3 Fatty Acids', '1 capsule (1000mg)'),
+    ('Nitric Oxide Boosters', '1 scoop (10g)'),
+    ('Taurine', '1 scoop (5g)'),
+    ('Vitamin C', '1000mg'),
+    ('Probiotics', '1 capsule');
