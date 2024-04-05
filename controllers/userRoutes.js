bb65034d9e0d3d8869127671f7e39e189d1670f1
dobeid <dialajo9@gmail.com>
2024-04-04 18:53:21 -0700

    fixed line 2

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models'); // might need to fix this line
+const { User } = require('./models'); 
 
 router.post('/', async (req, res) => {
   try {
