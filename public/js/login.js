1f71dd4cc7e34d9a16dbd200dafa81e2d5a480a8
dobeid <dialajo9@gmail.com>
2024-04-11 17:57:33 -0700

    changes to db folder, routes, handlebars

--- public/js/login.js
+++ public/js/login.js
@@ -4,20 +4,17 @@ const loginFormHandler = async (event) => {
   const email = document.querySelector('#email-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
 
-  // removed signup handler to be used in signup.js file
   if (email && password) {
     try {
-      const response = await fetch('/login', {
+      const response = await fetch('/api/user/login', {
         method: 'POST',
         body: JSON.stringify({ email, password }),
         headers: { 'Content-Type': 'application/json' },
       });
 
       if (response.ok) {
-        // Redirect to the homepage upon successful login
         document.location.replace('/');
       } else {
-        // Display an error message if login fails
         alert('Failed to log in. Please check your email and password.');
       }
     } catch (error) {
