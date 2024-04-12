90c8c1deb0658a91021dbd14c690a4a72385a285
FriedLikeRice <jacobleeferraro@gmail.com>
2024-04-11 17:30:05 -0700

    xcomit

--- public/js/login.js
+++ public/js/login.js
@@ -6,7 +6,7 @@ const loginFormHandler = async (event) => {
 
   if (email && password) {
     try {
-      const response = await fetch('/api/user/login', {
+      const response = await fetch('/api/userRoutes', {
         method: 'POST',
         body: JSON.stringify({ email, password }),
         headers: { 'Content-Type': 'application/json' },
