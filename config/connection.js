a15bdc55f95da7d9e3a21a6f8cb13d793a81dfc9
dobeid <dialajo9@gmail.com>
2024-04-18 20:20:02 -0700

    updated connection.js to fix error on render deployment

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  // For deployment on Heroku using JawsDB MySQL addon
+  // For deployment on Render using JawsDB MySQL addon
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   // For local development using MySQL database
@@ -13,7 +13,6 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: process.env.DB_HOST || 'localhost',
       dialect: 'mysql',
       port: process.env.DB_PORT || 3306
     }
